745a30561e5ebe5c28a08b9ceab5ddd300321f6f
Isabelle Bengrine <66420756+Isabellebengrine@users.noreply.github.com>
2021-09-10 14:08:53 +0200

    Update SyliusApi.php

--- src/Payum/SyliusApi.php
+++ src/Payum/SyliusApi.php
@@ -7,15 +7,42 @@ namespace Poliofu\SyliusClicAndPayPlugin\Payum;
 final class SyliusApi
 {
     /** @var string */
-    private $apiKey;
+    private $username;
 
-    public function __construct(string $apiKey)
+    /** @var string */
+    private $password;
+
+    /** @var string */
+    private $publicKey;
+
+    /** @var string */
+    private $sha256Key;
+
+    public function __construct(string $username, string $pass, string $key, string $sha256Key)
     {
-        $this->apiKey = $apiKey;
+        $this->username = $username;
+        $this->password = $pass;
+        $this->publicKey = $key;
+        $this->sha256Key = $sha256Key;
     }
 
-    public function getApiKey(): string
+    public function getUsername(): string
     {
-        return $this->apiKey;
+        return $this->username;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getPublicKey(): string
+    {
+        return $this->publicKey;
+    }
+
+    public function getSha256Key(): string
+    {
+        return $this->sha256Key;
     }
 }
