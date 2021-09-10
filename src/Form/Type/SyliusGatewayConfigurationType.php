e382c58d1d64eeb9d5faae0d9c5643c146f430a3
Isabelle Bengrine <66420756+Isabellebengrine@users.noreply.github.com>
2021-09-10 14:05:38 +0200

    Update SyliusGatewayConfigurationType.php

--- src/Form/Type/SyliusGatewayConfigurationType.php
+++ src/Form/Type/SyliusGatewayConfigurationType.php
@@ -7,11 +7,44 @@ namespace Poliofu\SyliusClicAndPayPlugin\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
-final class SyliusGatewayConfigurationType extends AbstractType
+final class ClicandpayGatewayConfigurationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('api_key', TextType::class);
+        $builder->add('username', TextType::class, [
+                    'label' => 'sylius.form.gateway_configuration.clicandpay.username',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'sylius.gateway_config.clicandpay.username.not_blank',
+                        ]),
+                    ],
+                ])
+                ->add('password', TextType::class, [
+                    'label' => 'sylius.form.gateway_configuration.clicandpay.password',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'sylius.gateway_config.clicandpay.username.not_blank',
+                        ]),
+                    ],
+                ])
+                ->add('publicKey', TextType::class, [
+                    'label' => 'sylius.form.gateway_configuration.clicandpay.publicKey',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'sylius.gateway_config.clicandpay.username.not_blank',
+                        ]),
+                    ],
+                ])
+                ->add('sha256Key', TextType::class, [
+                    'label' => 'sylius.form.gateway_configuration.clicandpay.sha256Key',
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'sylius.gateway_config.clicandpay.username.not_blank',
+                        ]),
+                    ],
+                ])
+        ;
     }
 }
